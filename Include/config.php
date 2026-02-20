d15a8a7437ac78a5fc92cc376ea55a0fbde2df81
zenlT <fnaticcy@gmail.com>
2026-02-20 15:44:52 +0200

    minor fixes

--- Include/config.php
+++ Include/config.php
@@ -1,1 +1,7 @@
 <?php
+declare(strict_types=1);
+define("APP_NAME", "University Module System");
+define("ROLE_ADMIN", "admin");
+define("ROLE_SUBMIT", "submit");
+define("ROLE_SEARCH", "search");
+define("MIN_USERNAME_LENGTH", 4);
