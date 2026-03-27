cb1ce66e7d6b9576d0710b43500f89a0005d8b58
zenlT <fnaticcy@gmail.com>
2026-03-27 14:09:09 +0200

    Add foreign key constraints for officer_name and officer_lastname

--- database/schema.sql
+++ database/schema.sql
@@ -25,4 +25,7 @@ officer_id int PRIMARY KEY,
 officer_name varchar(30),
 officer_lastname varchar(30),
 officer_position int,
-foreign key (officer_position) references positions(position_id));
+foreign key (officer_position) references positions(position_id)),
+foreign key (officer_name) references users(first_name),
+foreign key (officer_lastname) references users(last_name);
+
