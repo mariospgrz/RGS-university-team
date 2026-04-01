56a729cfd164329b8a292c0c5de6e7cff14ce7d0
Vafeiadis P <vafeiadisparaskevas@gmail.com>
2026-04-02 01:05:22 +0300

    Search feature v0.1

--- Search/Search.php
+++ Search/Search.php
@@ -1,5 +1,43 @@
 <?php
 require_once "../Include/header.php";
+Require_once "../Include/db.php";
+
+if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'User') {
+    header("Location: ../index.php");
+    exit;
+}
+
+$errors = [];
+if($_SERVER["REQUEST_METHOD"] === "POST") {
+    $search_query = trim($_POST['search_query'] ?? '');
+
+    if (empty($search_query)) {
+        $errors[] = "Πρέπει να εισάγετε όνομα ή επώνυμο ή θέση για αναζήτηση.";
+    }
+
+    if (empty($errors)) {
+        $sql = "SELECT first_name, last_name, 'Politician' AS source 
+        FROM users
+        WHERE (first_name LIKE :query OR last_name LIKE :query)
+        AND role = 'Politician'
+
+        UNION
+
+        SELECT users.first_name, users.last_name, positions.position AS source
+        FROM users
+        JOIN govofficers ON users.user_id = govofficers.user_id
+        JOIN positions ON govofficers.officer_position = positions.position_id
+        WHERE positions.position LIKE :query";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['query' => '%' . $search_query . '%']);
+
+        $results = $stmt->fetchAll(); }
+
+    
+
+}
+
 ?>
 
 <body>
@@ -9,7 +47,6 @@ require_once "../Include/header.php";
             <p>Πόθεν Έσχες</p>
             <nav>
                 <ul>
-                    <li><a href="Navigation.html">Search Tab</a></li>
                     <li><a href="Subscribe.html">Subscribe Tab</a></li>
                     <li><a href="Statistics.html">Statistics Tab</a></li>
                     <li><a href="../index.php" class="logout-btn">Logout</a></li>
@@ -18,7 +55,7 @@ require_once "../Include/header.php";
         </aside>
     <content class = "main-content">
         <h2>Search Page</h2>
-        <p> This is a simple page for search </p>
+        <p>  </p>
     </content>
     </div>
 </body>
